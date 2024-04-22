8ffd78a52200cea4c9c183d69547f739d9b0c57f
prasoon <jbprasoon@gmail.com>
2024-04-22 13:51:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+gsap.to(".imagecontainer", {
+    width: "100%",
+    duration: 2,
+    stagger: 2,
+    repeat: -1,
+})
+// gsap.to(".text h1", {
+
+//     top: 0,
+//     stagger: 4
+
+// })
+
+// gsap.to(".text h1", {
+//     delay: 1,
+//     top: "-100px",
+//     stagger: 2,
+//     repeat: -1,
+
+
+// })
+// gsap.to(".imagecontainer:nth-child(2)", {
+//     width: "0%",
+//     duration: 3,
+
+// })
